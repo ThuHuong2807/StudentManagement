de705289334a2134d017c24be25c15f84cc26f94
Đặng Thu Hương <thuhuongbk2807@gmail.com>
2020-10-23 10:17:31 +0700

    first commit

---
+++ src/Student/studentModify.java
@@ -0,0 +1,239 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * CRUD (insert, update, delete, findAll)
+ * @author Diep.Tran
+ */
+public class studentModify {
+    public static List<student> findAll() {
+        List<student> studentList = new ArrayList<>();
+        
+        Connection connection = null;
+        Statement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management", "root", "");
+            
+            //query
+            String sql = "select * from student";
+            statement = connection.createStatement();
+            
+            ResultSet resultSet = statement.executeQuery(sql);
+            
+            while (resultSet.next()) {                
+                student std = new student(resultSet.getInt("id"), 
+                        resultSet.getString("fullname"), resultSet.getString("gender"), 
+                        resultSet.getString("email"), resultSet.getString("phone_number"), 
+                        resultSet.getInt("age"));
+                studentList.add(std);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+        
+        return studentList;
+    }
+    
+    public static void insert(student std) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management", "root", "");
+            
+            //query
+            String sql = "insert into student(fullname, gender, age, email, phone_number) values(?, ?, ?, ?, ?)";
+            statement = connection.prepareCall(sql);
+            
+            statement.setString(1, std.getFullname());
+            statement.setString(2, std.getGender());
+            statement.setInt(3, std.getAge());
+            statement.setString(4, std.getEmail());
+            statement.setString(5, std.getPhoneNumber());
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static void update(student std) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management", "root", "");
+            
+            //query
+            String sql = "update student set fullname=?,gender=?,age=?,email=?,phone_number=? where id = ?";
+            statement = connection.prepareCall(sql);
+            
+            statement.setString(1, std.getFullname());
+            statement.setString(2, std.getGender());
+            statement.setInt(3, std.getAge());
+            statement.setString(4, std.getEmail());
+            statement.setString(5, std.getPhoneNumber());
+            statement.setInt(6, std.getId());
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static void delete(int id) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management", "root", "");
+            
+            //query
+            String sql = "delete from student where id = ?";
+            statement = connection.prepareCall(sql);
+            
+            statement.setInt(1, id);
+            
+            statement.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+    }
+    
+    public static List<student> findByFullname(String fullname) {
+        List<student> studentList = new ArrayList<>();
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            //lay tat ca danh sach sinh vien
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management", "root", "");
+            
+            //query
+            String sql = "select * from student where fullname like ?";
+            statement = connection.prepareCall(sql);
+            statement.setString(1, "%"+fullname+"%");
+            
+            ResultSet resultSet = statement.executeQuery();
+            
+            while (resultSet.next()) {                
+                student std = new student(resultSet.getInt("id"), 
+                        resultSet.getString("fullname"), resultSet.getString("gender"), 
+                        resultSet.getString("email"), resultSet.getString("phone_number"), 
+                        resultSet.getInt("age"));
+                studentList.add(std);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(studentModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //ket thuc.
+        
+        return studentList;
+    }
+}
+
